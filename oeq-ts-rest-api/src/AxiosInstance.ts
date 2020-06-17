669656600656955322ad2414e4c6cbf214b2879d
Christian Murphy <christian.murphy.42@gmail.com>
2020-06-16 20:33:12 -0700

    feat(types): enhance axios validation
    when a validator is passed to get it will return type from validator.
    when no validator is passed, type is unknown.

--- oeq-ts-rest-api/src/AxiosInstance.ts
+++ oeq-ts-rest-api/src/AxiosInstance.ts
@@ -2,7 +2,7 @@ import Axios, { AxiosResponse, AxiosError } from 'axios';
 import axiosCookieJarSupport from 'axios-cookiejar-support';
 import * as tough from 'tough-cookie';
 import { repackageError } from './Errors';
-import {stringify} from 'query-string';
+import { stringify } from 'query-string';
 
 // So that cookies work when used in non-browser (i.e. Node/Jest) type environments. And seeing
 // the oEQ security is based on JSESSIONID cookies currently this is key.
@@ -22,16 +22,26 @@ const catchHandler = (error: AxiosError | Error): never => {
  * @param queryParams The query parameters to send with the GET request
  * @param transformer A function which returns a copy of the raw data from the GET with any required values transformed - this should NOT mutate the input data (transforms should start on a copy/clone of the input)
  */
-export const GET = <T>(
+export function GET<T>(path: string): Promise<unknown>;
+export function GET<T>(
   path: string,
-  validator?: (data: unknown) => boolean,
+  validator: (data: unknown) => data is T,
   queryParams?: object,
-  transformer?: (data: unknown) => T,
-): Promise<T> =>
-  axios
-    .get(path, {params: queryParams, paramsSerializer: params => stringify(params)}, )
-    .then((response: AxiosResponse) => {
-      const data: any = transformer? transformer(response.data) : response.data;
+  transformer?: (data: unknown) => T
+): Promise<T>;
+export function GET<T>(
+  path: string,
+  validator?: (data: unknown) => data is T,
+  queryParams?: object,
+  transformer?: (data: unknown) => T
+): Promise<T> | Promise<unknown> {
+  return axios
+    .get(path, {
+      params: queryParams,
+      paramsSerializer: (params) => stringify(params),
+    })
+    .then((response: AxiosResponse<unknown>) => {
+      const data = transformer ? transformer(response.data) : response.data;
       if (validator && !validator(data)) {
         // If a validator is provided, but it fails to validate the provided data...
         throw new Error('Data format mismatch with data received from server.');
@@ -40,11 +50,13 @@ export const GET = <T>(
       return data;
     })
     .catch(catchHandler);
+}
 
-export const PUT = <T, R>(path: string, data?: T): Promise<R> =>
-  axios
+export function PUT<T, R>(path: string, data?: T): Promise<R> {
+  return axios
     .put(path, data)
     .then((response: AxiosResponse<R>) => response.data)
     .catch(catchHandler);
+}
 
 export default axios;
